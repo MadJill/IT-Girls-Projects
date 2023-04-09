5616ff015b9cf1d6875a564b1b5295987c274768
Rushana Alieva <106332841+MadJill@users.noreply.github.com>
2023-04-09 20:39:15 +0200

    week 11

---
+++ Sandbox/assets/script/style.js
@@ -0,0 +1,15 @@
+let result = sum(7, 8);
+console.log(result);
+function sum(a, b){
+    return a + b;
+}
+
+// alert(result);
+
+ let sub = function (x,y){
+    return x-y;
+ };
+ let total = sub(15, 9);
+ console.log(total);
+
+//  
