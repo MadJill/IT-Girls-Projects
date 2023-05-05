bc43e439e7784edb9653cafe3e8b3e1dc5781264
Rushana Alieva <106332841+MadJill@users.noreply.github.com>
2023-05-05 11:17:48 +0200

    Raw Fish CarCalc

---
+++ HomeWorks/Week 16/Raw Fish/assets/script/script.js
@@ -0,0 +1,10 @@
+const renaultModels = [
+    {name:`simbol`, price:100},
+    {name:`logan`, price:120},
+    {name:`megane`, price:150}
+]
+const brand = document.getElementById(`brand`);
+
+const onBrandChange = () => {
+debugger 
+}
